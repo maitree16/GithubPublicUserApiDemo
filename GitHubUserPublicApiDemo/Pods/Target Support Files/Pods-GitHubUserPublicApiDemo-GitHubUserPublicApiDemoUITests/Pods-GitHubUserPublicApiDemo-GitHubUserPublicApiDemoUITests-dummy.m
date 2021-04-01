28a33fc91d7eac322801826e7493e92f2170f775
maitree thaker <maitreethaker96@gmail.com>
2021-04-01 10:33:53 -0700

    committed

---
+++ GitHubUserPublicApiDemo/Pods/Target Support Files/Pods-GitHubUserPublicApiDemo-GitHubUserPublicApiDemoUITests/Pods-GitHubUserPublicApiDemo-GitHubUserPublicApiDemoUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GitHubUserPublicApiDemo_GitHubUserPublicApiDemoUITests : NSObject
+@end
+@implementation PodsDummy_Pods_GitHubUserPublicApiDemo_GitHubUserPublicApiDemoUITests
+@end
